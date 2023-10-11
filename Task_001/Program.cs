82e9cffffa69794a98e921ed576a56ba415c064d
Алексей Суслов <suslovwow@yandex.ru>
2023-10-11 18:30:44 +0300

    сдал автотесты, тут перемешенные решения

---
+++ Task_001/Program.cs
@@ -0,0 +1,45 @@
+﻿// using System;
+
+// public class Answer {
+//     static void CompareNumbers(int firstNumber, int secondNumber)
+//     {
+//         // Введите свое решение ниже
+//       if ( firstNumber < secondNumber)
+//       Console.WriteLine("Первое число " + firstNumber + " меньше чем второе число " + secondNumber);
+//       if ( firstNumber > secondNumber)
+//       Console.WriteLine("Первое число " + firstNumber + " больше чем второе число " + secondNumber);
+//       if ( firstNumber == secondNumber)
+//       Console.WriteLine("Введены числа равные " + "'"+firstNumber+"'");
+//     }
+
+
+//   // Не удаляйте и не меняйте метод Main! 
+//     static public void Main(string[] args) {
+//         int firstNumber, secondNumber;
+
+//         if (args.Length >= 2) {
+//             firstNumber = int.Parse(args[0]);
+//             secondNumber = int.Parse(args[1]);
+//         } else {
+//            // Здесь вы можете поменять значения для отправки кода на Выполнение
+//             firstNumber = 1;
+//             secondNumber = 1;
+//         }
+
+//         // Не удаляйте строки ниже
+//         CompareNumbers(firstNumber, secondNumber);
+//     }
+// }
+
+// задача 2
+Console.WriteLine("Введите первое число: ");
+double firstScore1  = Convert.ToDouble (Console.ReadLine ());
+Console.WriteLine("Введите второе число: ");
+double secondScore2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+    if(firstScore1 < secondScore2)
+    Console.WriteLine(firstScore1 + " Минимальное число");
+    Console.WriteLine (secondScore2 + " максимальное число");
---
+++ Task_004/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= number)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+}
+
+//  for (int count = 2; count <= number; count += 2)
+//  {
+//   Console.Write($"{count}\t"); 
